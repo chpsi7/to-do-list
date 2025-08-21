f49eae28046f35888d483d54db09b254a89bcf9b
Chopngo <chomsa7@gmail.com>
2025-08-21 10:17:42 +0530

    fix the errors regarding adding tasks

--- script.js
+++ script.js
@@ -8,21 +8,22 @@ const todosContainer = document.querySelector('.todos-container');
 
 const taskList = document.querySelector('#task-list');
 
-addTaskBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    const event = taskInput.value.trim();
+addTaskBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const taskTest = taskInput.value.trim();
     
-    if (!event) {
+    if (!taskTest) {
         return;
     }
-    displayEvents(event);
+    displayTasks(taskTest);
+    taskInput.value = '';
 });
 
-function displayEvents(event) {
+function displayTasks(taskTest) {
     const li = document.createElement('li');
-    li.textContent = event;
-    taskList.appendChild('li');
+    li.textContent = taskTest;
+    taskList.appendChild(li);
 
     const img = document.getElementById('no-todos');
-    img.style.display == 'none';
+    img.style.display = 'none';
 }
